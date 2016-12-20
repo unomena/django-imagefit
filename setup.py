a7dbd3021ad93d73109c29bea4a6d43dde7a7c44
Euan Jonker <euan@unomena.com>
2016-12-20 19:45:42 +0200

    Ignore the long description in the setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,6 @@ setup(
     name='django-imagefit',
     version='0.4.4',
     description='Render an optimized version of your original image on display. Ability to resize and crop.',
-    long_description=open('README.md').read(),
     author='Vincent Agnano',
     author_email='vincent.agnano@scopyleft.fr',
     url='http://github.com/vinyll/django-imagefit',
